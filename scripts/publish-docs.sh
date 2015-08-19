4ba91ca842cfd8387bfc99a7a7555a613d5e0444
Michael Jackson <mjijackson@gmail.com>
2015-08-19 15:43:39 -0700

    Update publish script

--- scripts/publish-docs.sh
+++ scripts/publish-docs.sh
@@ -3,11 +3,14 @@
 printf "Which tag do you want to build the docs for? "
 read tag
 
-git checkout $tag -- docs
+sha=`git rev-list -1 $tag`
+
+rm -rf docs
+git checkout $sha -- docs
 git reset docs
 
-cp docs/README.md SUMMARY.md
-cp -r docs/* .
+mv docs/README.md SUMMARY.md
+mv docs/* .
 
 gitbook build
 
@@ -17,4 +20,6 @@ mv _book $tag
 git add $tag
 git commit -m "Update $tag docs"
 
-#git push git@github.com:rackt/history gh-pages
+git push git@github.com:rackt/history gh-pages
+
+git clean -f .
