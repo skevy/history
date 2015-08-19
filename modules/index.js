adb5f6dd512cfed7b3ec9a3dccb50c1e2a9efebd
Michael Jackson <mjijackson@gmail.com>
2015-08-19 13:56:38 -0700

    Add redirect index page

--- modules/index.js
+++
@@ -1,8 +0,0 @@
-export createHistory from './createBrowserHistory';
-export createHashHistory from './createHashHistory';
-export createMemoryHistory from './createMemoryHistory';
-export createLocation from './createLocation';
-
-export enableQueries from './enableQueries';
-
-export Actions from './Actions';
